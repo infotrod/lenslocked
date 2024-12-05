05bf129c5ec0fc9687306536d7f3865e7b6628d3
infotrod <info@infotrod.com>
2024-12-05 00:45:30 -0600

    Added first gohtml template

---
+++ cmd/exp/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age  int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	// user := struct {
+	// 	Name string
+	// }{
+	// 	Name: "John Smith",
+	// }
+
+	user := User{
+		Name: "Rustem Sharipov",
+		Age:  48,
+		Meta: UserMeta{
+			Visits: 4,
+		},
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
