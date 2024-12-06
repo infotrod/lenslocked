351c5cc6f45b431197acf4cd028862e02c1d1f5b
infotrod <info@infotrod.com>
2024-12-06 13:43:14 -0600

    Created 'User' handler and template for rendering it as well as corresponding data type. Eventually solved all four exercises Jon requested

--- main.go
+++ main.go
@@ -11,7 +11,19 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
+type Course struct {
+	LanguageName string
+	Price        float64
+}
+type User struct {
+	Name             string
+	Email            string
+	SubscriptionEnds string
+	Courses          []Course
+	IsPrimaryMember  bool
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string, data any) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
@@ -19,7 +31,7 @@ func executeTemplate(w http.ResponseWriter, filepath string) {
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
+	err = tpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
@@ -29,23 +41,42 @@ func executeTemplate(w http.ResponseWriter, filepath string) {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
+	executeTemplate(w, tplPath, nil)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
+	executeTemplate(w, tplPath, nil)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
+	executeTemplate(w, tplPath, nil)
 }
 
 func productHandler(w http.ResponseWriter, r *http.Request) {
 	productId := chi.URLParam(r, "productID")
 	w.Write([]byte(fmt.Sprintf("<p>Product id = %s</p>", productId)))
 	// fmt.Fprint(w, fmt.Sprintf("<h1>Product id = %s</h1>", productId))
+}
+
+func userHadler(w http.ResponseWriter, r *http.Request) {
+	courses := []Course{
+		{LanguageName: "Go", Price: 29.99},
+		{LanguageName: "Python", Price: 19.99},
+		{LanguageName: "JavaScript", Price: 24.99},
+	}
+
+	user := User{
+		Name:             "Rustem Sharipov",
+		Email:            "sharipov.rustem@gmail.com",
+		SubscriptionEnds: "03/01/2025",
+		Courses:          courses,
+		IsPrimaryMember:  true,
+	}
+
+	tplPath := filepath.Join("templates", "user.gohtml")
+	executeTemplate(w, tplPath, user)
 }
 
 func main() {
@@ -58,6 +89,7 @@ func main() {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	r.Get("/product/{productID}", productHandler)
+	r.Get("/user", userHadler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
