20e2a3631be6b8e8a15a9f82cc3a2c9feb243355
infotrod <info@infotrod.com>
2024-12-08 21:29:55 -0600

    Created a view package

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/infotrod/lenslocked/views"
 )
 
 type Course struct {
@@ -24,19 +24,13 @@ type User struct {
 }
 
 func executeTemplate(w http.ResponseWriter, filepath string, data any) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, data)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, data)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
